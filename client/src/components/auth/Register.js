872ad63e1e4b06f0a3d84291223077d27a16eefe
Kal <info@kalacademy.org>
2020-10-04 11:11:50 -0700

    redux end to end

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
 import classnames from 'classnames';
+import {registerUser} from '../../actions/authActions';
 
 class Register extends Component {
   constructor(){
@@ -31,10 +33,15 @@ class Register extends Component {
       password2: this.state.password2
     };
 
-    axios
-      .post('/api/users/register', newUser)
-      .then(res => console.log(res.data))
-      .catch(err => this.setState({errors: err.response.data}));
+    this.props.registerUser(newUser, this.props.history);
+
+
+  }
+
+  componentWillReceiveProps(nextProps){
+    if (nextProps.errors){
+      this.setState({errors: nextProps.errors});
+    }
   }
 
   render() {
@@ -124,4 +131,15 @@ class Register extends Component {
   }
 }
 
-export default Register;
+Register.propTypes = {
+  registerUser: PropTypes.func.isRequired,
+  errors: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => (
+  {
+    errors: state.errors
+  }
+);
+
+export default connect(mapStateToProps, {registerUser})(Register);
