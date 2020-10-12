0d845a54a74af5c8c2773a00692ae3769df24a45
Kal <info@kalacademy.org>
2020-10-12 12:54:57 -0700

    Complete client code

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import jwt_decode from 'jwt-decode';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Footer from './components/layout/Footer';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
@@ -13,6 +13,19 @@ import { logoutUser } from './actions/authActions';
 import setAuthToken from './utils/setAuthToken';
 import {SET_USER} from './actions/types';
 
+import PrivateRoute from "./components/common/PrivateRoute";
+
+import { clearCurrentProfile } from "./actions/profileActions";
+import Dashboard from "./components/dashboard/Dashboard";
+import CreateProfile from "./components/create-profile/CreateProfile";
+import EditProfile from "./components/edit-profile/EditProfile";
+import AddExperience from "./components/add-credentials/AddExperience";
+import AddEducation from "./components/add-credentials/AddEducation";
+import Profiles from "./components/profiles/Profiles";
+import Profile from "./components/profile/Profile";
+import Posts from "./components/posts/Posts";
+import Post from "./components/post/Post";
+import NotFound from "./components/not-found/NotFound";
 
 if (localStorage.jwtToken){
   //decode
@@ -36,7 +49,6 @@ if (localStorage.jwtToken){
   });
 }
 class App extends Component {
-
   render() {
     return (
       <Provider store={store}>
@@ -44,8 +56,50 @@ class App extends Component {
           <div className="App">
             <Navbar />
             <Route exact path="/" component={Landing} />
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/login" component={Login} />
+            <div className="container">
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/profiles" component={Profiles} />
+              <Route exact path="/profile/:handle" component={Profile} />
+              <Switch>
+                <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/create-profile"
+                  component={CreateProfile}
+                />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/edit-profile"
+                  component={EditProfile}
+                />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/add-experience"
+                  component={AddExperience}
+                />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/add-education"
+                  component={AddEducation}
+                />
+              </Switch>
+              <Switch>
+                <PrivateRoute exact path="/feed" component={Posts} />
+              </Switch>
+              <Switch>
+                <PrivateRoute exact path="/post/:id" component={Post} />
+              </Switch>
+              <Route exact path="/not-found" component={NotFound} />
+            </div>
             <Footer />
           </div>
         </Router>
