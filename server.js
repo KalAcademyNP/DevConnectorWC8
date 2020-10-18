8ac5f288c92fbb6f023f14a757e19f676161b503
Kal <info@kalacademy.org>
2020-10-18 11:04:26 -0700

    prepared to deploy

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyparser = require('body-parser');
 const passport = require('passport');
+const path = require('path');
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
@@ -19,7 +20,15 @@ app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 
-const port = 7000;
+if (process.env.NODE_ENV === 'production'){
+  app.use(express.static('client/build'));
+  app.get('*', (req,res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html' ))
+  });
+}
+
+
+const port = process.env.PORT || 7000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
 
 //Db config
